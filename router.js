7f55baa81d45aac1fd3dfbac834074582a3360e6
zomboyd <alecko.bc@gmail.com>
2015-03-18 12:37:07 +0100

    add requestHandler

--- router.js
+++ router.js
@@ -1,6 +1,10 @@
-function route(pathname)
+function route(pathname, handle)
 {
     console.log("Debut du traitement de l'URL" + pathname + ".");
+    if (typeof handle[pathname] === 'function')
+      handle[pathname]();
+    else
+      console.log(pathname + " non pris en charge.");
 };
 
 exports.route = route;
--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 var http = require("http");
 var url = require("url");
 
-function start(route)
+function start(route, handle)
 {
     function onReq(req, resp)
     {
-	var pathname = url.parse(req.url).pathname;
-	console.log("requete pour le chemin " + pathname + " recu.");
-	route(pathname);
-	resp.writeHead(200, {"Content-Type": "text/plain"});
-	resp.write("Hello!");
-	resp.end();
-    };    
+      var pathname = url.parse(req.url).pathname;
+       console.log("requete pour le chemin " + pathname + " recu.");
+       route(pathname, handle);
+       resp.end();
+    };
     http.createServer(onReq).listen("8888");
     console.log("Server starts");
 }
