2fa7c7581de108553f6cc7a3b5209f5c5ffc1a5b
zomboyd <alecko.bc@gmail.com>
2015-03-18 12:35:30 +0100

    add handler

---
+++ requestHandler.js
@@ -0,0 +1,12 @@
+function start()
+{
+  console.log("Le gestionnaire 'start' est appelé.");
+}
+
+function upload()
+{
+  console.log("Le gestionnaire 'upload' est appelé.");
+}
+
+exports.start = start;
+exports.upload = upload;
